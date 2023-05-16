adecc9d6241355c05f2dd8f2c0a440c3d4ae67c4
Nishant Shah <nishantcshah13@gmail.com>
2023-05-16 12:47:45 +0530

    fix: css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/main.css';
 
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 
 createApp(App).mount('#app')
--- src/presentation/pages/Login.vue
+++ src/presentation/pages/Login.vue
@@ -13,19 +13,11 @@
                 <input type="password" class="form-control form-control-lg" />
             </div>
  
-            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+            <button type="submit" class="btn btn-outline-dark btn-lg btn-block">Sign In</button>
  
             <p class="forgot-password text-right mt-2 mb-4">
                 <router-link to="/forgot-password">Forgot password ?</router-link>
             </p>
- 
-            <div class="social-icons">
-                <ul>
-                    <li><a href="#"><i class="fa fa-google"></i></a></li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                </ul>
-            </div>
  
         </form>
     </div>
